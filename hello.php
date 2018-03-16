5f4afb61a4253b5b81af00277155918b48aac57e
Dmitriy <solleks777@gmail.com>
2018-03-17 07:39:55 +0800

    Создан перк *Сила*

--- hello.php
+++ hello.php
@@ -1,11 +1,15 @@
 <?
 
 abstract class Unit {
-	
+	protected $power;
+	protected $speed;
+	protected $intellekt;
 }
 
 class Perks {
-	
+	function absolutePower() {
+		return "power +1";
+	}
 }
 
 ?>
