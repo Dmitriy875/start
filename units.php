ff203e5d48f491af381ccafce4cc4347732c921c
Dmitriy <solleks777@gmail.com>
2018-03-17 22:25:24 +0800

    Создал абстрактные классы Unit и Perk

--- units.php
+++ units.php
@@ -1,6 +1,40 @@
+<!DOCTYPE=html>
+<html>
+<head>
+<meta charset = 'utf-8'>
+</head>
+<p>
 <?
 
-abstract class Unit {}
+abstract class Unit {
+	private $experience	= 0;
+	private $power		= 0;
+	private $agility	= 0;
+	private $intellect	= 0;
+	private $name;
+	
+	function __construct( $name ) {
+		$this->power	= $power;
+		$this->name		= $name;
+		
+	}
+	function getExperience() {
+		print "Unit experience = " 	. $this->experience . "\n";
+	}
+	function getPower() {
+		print "Unit power = " 		. $this->power . "\n";
+	}
+	function getAgility() {
+		print "Unit agility = " 	. $this->agility . "\n";
+	}
+	function getIntellect() {
+		print "Unit intellect = " 	. $this->intellect . "\n";
+	}
+	function getName() {
+		print "Unit name = " 		. $this->name . "\n";
+	}
+	
+}
 
 class Knight extends Unit {}
 
@@ -8,6 +42,25 @@ class Cavalry extends Unit {}
 
 class Archer extends Unit{}
 
-class Canos extends Unit{}
+class Canon extends Unit{}
+
+abstract class Perk {}
+class PowerPerk extends Perk{
+	function addPower( Unit $unit ) {
+		return $unit->getPower ($this) + 11;
+	}
+}
+
+$skill = new PowerPerk;
+
+$warrior = new Knight( "Василий the Brave" );
+$warrior->getPower();
+$warrior->getAgility();
+$warrior->getIntellect();
+$warrior->getExperience();
+$warrior->getName();
+
 
 ?>
+</p>
+</html>
