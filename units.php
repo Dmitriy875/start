ea5184803dc524c562789628ee817e48db7f1d46
Dmitriy <solleks777@gmail.com>
2018-03-18 20:59:47 +0800

    Перки работают, но мне не нравится их вызов в конструкторе

--- units.php
+++ units.php
@@ -1,37 +1,31 @@
-<!DOCTYPE=html>
-<html>
-<head>
-<meta charset = 'utf-8'>
-</head>
-<p>
 <?
 
 abstract class Unit {
 	private $experience	= 0;
-	private $power		= 0;
+	private $power		= 10;
 	private $agility	= 0;
-	private $intellect	= 0;
+	private $intellect	= 80;
 	private $name;
 	
-	function __construct( $name ) {
-		$this->power	= $power;
-		$this->name		= $name;
-		
+	function __construct( $name, Perk $perk, IntellectPerk $intell ) {
+		$this->name			= $name;
+		$this->power		= $this->power + $perk->power;
+		$this->intellect	= $this->intellect + $intell->intellect;
 	}
 	function getExperience() {
-		print "Unit experience = " 	. $this->experience . "\n";
+		print "Unit experience	 	= " 	. $this->experience . "\n";
 	}
 	function getPower() {
-		print "Unit power = " 		. $this->power . "\n";
+		print "Unit power			= "		. $this->power . "\n";
 	}
 	function getAgility() {
-		print "Unit agility = " 	. $this->agility . "\n";
+		print "Unit agility 		= " 	. $this->agility . "\n";
 	}
 	function getIntellect() {
-		print "Unit intellect = " 	. $this->intellect . "\n";
+		print "Unit iq 			= " 	. $this->intellect . "\n";
 	}
 	function getName() {
-		print "Unit name = " 		. $this->name . "\n";
+		print "Unit name 			= " 		. $this->name . "\n";
 	}
 	
 }
@@ -44,23 +38,22 @@ class Archer extends Unit{}
 
 class Canon extends Unit{}
 
-abstract class Perk {}
-class PowerPerk extends Perk{
-	function addPower( Unit $unit ) {
-		return $unit->getPower ($this) + 11;
-	}
+abstract class Perk {
+	
 }
 
-$skill = new PowerPerk;
+class PowerPerk extends Perk{
+	public $power = 100;
+}
+class IntellectPerk extends Perk {
+	public $intellect = 20;
+}
 
-$warrior = new Knight( "Василий the Brave" );
+$warrior = new Knight( "Василий *the Brave one* Рыцарь", new PowerPerk, new IntellectPerk );
 $warrior->getPower();
 $warrior->getAgility();
 $warrior->getIntellect();
 $warrior->getExperience();
 $warrior->getName();
 
-
 ?>
-</p>
-</html>
